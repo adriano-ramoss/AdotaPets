b5c24456acc31716e5287dc1c7fae48224c012b7
adriano-ramoss <adrianoramos2401@gmail.com>
2023-03-09 19:12:57 -0300

    teste

---
+++ app/src/main/java/br/com/etecia/adotapets/MenuPricipal_Activity.java
@@ -0,0 +1,14 @@
+package br.com.etecia.adotapets;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MenuPricipal_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.menu_pricipal_layout);
+    }
+}
